d28f7263b6b49ca4976d9d89e485a38bf0a998ad
claes.lundahl@gmail.com <claes.lundahl@gmail.com@b7768173-3cae-474d-33ed-934f06e8b460>
2013-11-05 10:21:25 +0000

    [maven-release-plugin]  copy for tag innovationsslussen-theme-system-1.4

---
+++ tags/innovationsslussen-theme-system-1.4/core-bc/modules/theme/src/main/webapp/js/theme-main.js
@@ -0,0 +1,31 @@
+// Plain javascript that runs before AUI is ready (to prevent content flashing
+
+// Body
+addCssClassName(document.body, 'js');
+
+////Hide footer before positionFooter has been run
+//var plainOldJsfooterNode = document.getElementById('footer');
+//addCssClassName(plainOldJsfooterNode, 'aui-helper-hidden');
+
+function addCssClassName(node, cssClassName) {
+	if(node) {
+		var newClassName = node.className + ' ' + cssClassName;
+		node.className = newClassName;
+	}
+}
+
+AUI().ready('innovationsslussen-theme', function(A) {
+	
+	var innovationsslussenTheme = new A.InnovationsslussenTheme().render();
+});
+
+/*
+Liferay.on('allPortletsReady',function() {
+	AUI().use('aui-base', 'rp-footer-plugin', function(A) {
+		var footerNode = A.one('#footer');
+		if(footerNode) {
+			footerNode.plug(A.Plugin.RpFooter);	
+		}
+	});
+});
+*/
